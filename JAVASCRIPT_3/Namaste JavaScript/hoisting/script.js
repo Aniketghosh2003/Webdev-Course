9adb942bcc0f9709a4053d5e74f5dc713e00d0e2
Aniket Ghosh <121872510+Aniketghosh2003@users.noreply.github.com>
2025-02-16 15:39:18 +0530

    Javascript learning form angela Yu course from udemy.
    It is only Learning purposes.

---
+++ JAVASCRIPT_3/Namaste JavaScript/hoisting/script.js
@@ -0,0 +1,21 @@
+// var x = 7;
+
+// function getname(){
+//     console.log("Ankiet Ghosh");
+// }
+
+// getname();
+// console.log(x);
+
+
+
+
+getname();
+console.log(x);
+
+var x = 7;
+
+function getname(){
+    console.log("Ankiet Ghosh");
+}
+
---
+++ JAVASCRIPT_3/index.js
@@ -0,0 +1,49 @@
+// var message = "Hello";
+// var name = "Aniket"
+// console.log(message + " , there , " + name);
+const prompt = require('prompt-sync')();
+// var paragraph;
+// paragraph = prompt("Enter the paragraph:-");
+// var len = paragraph.length;
+// var diff = 140 - len;
+// console.log("You have written "+ len +" words and the remaining is "+ diff);
+// console.log(Math.floor(4.3));
+
+// prompt("Enter the name of the first person ?");
+// prompt("Enter the name of the second person ?");
+// var n  = Math.random();
+// var per = Math.floor(n*100) + 1;
+// console.log(per% love);
+// var arr = [];
+// var i = 1;
+// function fizzbuzz() {
+//     if (i % 3 === 0) {
+//         var str = "fizz";
+//         arr.push(str);
+//     }
+//     else if (i % 5 === 0) {
+//         var str = "bizz";
+//         arr.push(str);
+//     }
+//     else if (i % 3 === 0 && i % 5 === 0) {
+//         var str = "fizzbizz";
+//         arr.push(str);
+//     }
+//     else {
+//         arr.push(i);
+//     }
+//     i++;
+//     console.log(arr);
+// }
+
+// fizzbuzz();
+// fizzbuzz();
+// fizzbuzz();
+// fizzbuzz();
+// fizzbuzz();
+// fizzbuzz();
+// fizzbuzz();
+// fizzbuzz();
+// fizzbuzz();
+// fizzbuzz();
+
